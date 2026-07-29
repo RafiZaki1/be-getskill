692c760f40647805ce94d59748197c7a3d9e353e
Dwisetyoramadhani <dsrtyo11@gmail.com>
2026-07-29 11:43:16 +0700

    feat: add CourseRepository implementation extending BaseRepository

---
+++ app/Contracts/Repositories/BaseRepository.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Contracts\Repositories;
+
+use Illuminate\Database\Eloquent\Model;
+
+abstract class BaseRepository
+{
+    /**
+     * Handle model initialization.
+     *
+     * @var Model $model
+     */
+
+    public Model $model;
+    public Model $model2;
+}
